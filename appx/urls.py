ba8d22a8caa949c77b74d0f4c4d2e707f0b54b47
IGxRohit <creative07vibez@gmail.com>
2024-05-16 14:41:59 +0530

    k

--- appx/urls.py
+++ appx/urls.py
@@ -5,6 +5,8 @@ from appx import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('contact/',views.contact,name="contact"),
-    path('savedata',views.savedata,name="savedata")
+    path('savedata',views.savedata,name="savedata"),
+    path('services/',views.services,name="services"),
+    path('delete/<int:id>',views.delete,)
 
 ]
