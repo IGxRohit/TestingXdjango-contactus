9dc1844fc31c85f7a6889fc11404662df7988303
IGxRohit <creative07vibez@gmail.com>
2024-05-16 14:55:32 +0530

    j

--- appx/urls.py
+++ appx/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('contact/',views.contact,name="contact"),
     path('savedata',views.savedata,name="savedata"),
-<<<<<<< HEAD
     path('services/',views.services,name="services"),
-    path('delete/<int:id>',views.delete,)
-=======
-    path('services/',views.services,name="services")
->>>>>>> 15e090cdaf9aac0b34ba82e0366fa3ab391b7393
+    path('delete/<int:id>',views.delete)
 
 ]
